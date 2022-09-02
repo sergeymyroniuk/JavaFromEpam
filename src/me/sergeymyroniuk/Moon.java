e112d3cfaf268a28408c7b4a786fc159badc0768
sergeymyroniuk <76446909+sergeymyroniuk@users.noreply.github.com>
2022-09-02 13:13:27 +0300

    Add files via upload

---
+++ src/me/sergeymyroniuk/Moon.java
@@ -0,0 +1,9 @@
+
+public class Moon {
+    public static void main(String args[]) {
+        double earthweight = 165; //вес на Земле
+        double moonweight; // вес на Луне
+        moonweight = earthweight * 0.17;
+        System.out.println(earthweight + " земных фунтов эквивалентны " + moonweight + " лунным фунтам.");
+    }
+}
