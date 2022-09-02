c3b44e00612ad190023062773a97f39c5d1c39c9
sergeymyroniuk <76446909+sergeymyroniuk@users.noreply.github.com>
2022-09-02 13:15:06 +0300

    Add files via upload

---
+++ src/me/sergeymyroniuk/Lab0102-Inches/src/ConversionTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ConversionTable {
+    public static void main ( String args[] ) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a value for inch: ");
+        double inch = input.nextDouble();
+        double meters = inch * 0.0254;
+        System.out.println(inch + " inch is " + meters + " meters");
+
+    }
+}
+
